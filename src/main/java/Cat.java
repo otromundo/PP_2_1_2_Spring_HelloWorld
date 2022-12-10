71532249ff39b87cab3b483216c72c1a74801619
Aleksei Vakhrin <aavakhrin@gmail.com>
2022-12-10 21:25:37 +0400

    Task completed

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    final private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public void say() {
+        System.out.println(name + ": meow");
+    }
+}
